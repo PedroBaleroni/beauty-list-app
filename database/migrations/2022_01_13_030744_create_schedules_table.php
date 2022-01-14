86a247c3db747e090a0a2a5668d4485dffb3d825
PedroBaleroni <pedrobaleroni@alunos.utfpr.edu.br>
2022-01-13 23:22:05 -0300

    routes and controllers

---
+++ database/migrations/2022_01_13_030744_create_schedules_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('service_id');
+            $table->foreign('service_id')->references('id')->on('services');
+            $table->unsignedBigInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('users');
+            $table->timestamp('service_schedule')->default(now());
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(
+    [
+        'as' => 'schedule.',
+        'prefix' => 'agenda',
+        'middleware' => 'auth'
+    ],  function(){
+        Route::get('/meus-horarios','ScheduleController@index')->name('index');
+        Route::get('/clientes','ScheduleController@manager')->name('admin');
+        Route::post('/set','ScheduleController@setSchedule')->name('set');
+        Route::post('/store','ScheduleController@store')->name('store');
+        Route::get('/{id?}','ScheduleController@show')->name('show');
+    }
+);
+
+Route::group(
+    [
+        'as' => 'product',
+        'prefix'=>'estoque',
+        'middleware'=>'auth'
+    ],  function(){
+        Route::get('/', 'ProductsController@index')->name('index');
+        Route::post('/store','ProductsController@store')->name('store');
+    }
+);
+
+Route::group(
+    [
+        'as'=>'service',
+        'prefix' => 'servico',
+        'middleware' => 'auth' 
+    ],  function(){
+        Route::get('/', 'ServicesController@index')->name('index');
+        Route::get('/{id?}', 'ServicesController@show')->name('show');
+        Route::post('/store','ServicesController@store')->name('store');
+    }
+);
